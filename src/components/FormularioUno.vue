47b22f5233f4612b79b6e0c2bf5c854be7e1a250
carlosdizx <carlodiaz@umariana.edu.co>
2021-06-07 19:32:39 -0500

    Creamos el formulario para la persona uno

---
+++ src/components/FormularioUno.vue
@@ -0,0 +1,54 @@
+<template>
+  <form>
+    <input
+      type="text"
+      class="form-control mt-2"
+      placeholder="Ingrese nommbre"
+      v-model="personaUno.nombre"
+    />
+    <input
+        type="number"
+        class="form-control mt-2"
+        placeholder="Ingrese documento"
+        v-model.number="personaUno.documento"
+    />
+    <div class="form-check form-check-inline">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        id="check-1"
+        v-model="personaUno.accion"
+        value="Representante"
+      />
+      <label class="form-check-label" for="check-1">Representante</label>
+    </div>
+    <div class="form-check form-check-inline">
+      <input
+          class="form-check-input"
+          type="checkbox"
+          id="check-2"
+          v-model="personaUno.accion"
+          value="Encargado"
+      />
+      <label class="form-check-label" for="check-2">Encargado</label>
+    </div>
+  </form>
+  <p>{{personaUno}}</p>
+</template>
+
+<script>
+export default {
+  name: "FormularioUno",
+  data() {
+    return {
+      personaUno: {
+        nombre: "",
+        documento: 0,
+        accion: []
+      }
+    };
+  }
+};
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <FormularioUno />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import FormularioUno from "@/components/FormularioUno";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    FormularioUno
   }
-}
+};
 </script>
